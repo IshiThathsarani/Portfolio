5fb965fac749a74a4cc6bda35cb9e6e371fc728c
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-03-16 20:39:23 +0530

    update the profile

--- src/App.js
+++ src/App.js
@@ -26,7 +26,31 @@ function App() {
             <img src = {profilepic} alt="profile photo" id="dp"/>
           </div>
       </div>
-      
+      <div id="about">
+                <div id="header">
+                    <svg xmlns="http://www.w3.org/2000/svg"  aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 64 64" >
+                        <circle cx="32" cy="32" r="30" fill="#ed4c5c" ></circle></svg>
+                    &nbsp;
+                    <svg xmlns="http://www.w3.org/2000/svg"  aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 36 36" >
+                        <circle cx="18" cy="18" r="18" fill="#FDCB58"></circle></svg>
+                    &nbsp;
+                    <svg xmlns="http://www.w3.org/2000/svg"  aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 36 36"  >
+                        <circle cx="18" cy="18" r="18" fill="#78B159"></circle></svg>
+                    <br/>
+                
+                <div id="details">
+                    <p>
+                    Hi :
+                    <br/>
+                    <br/>
+                    I'm Ishini Siriwardena. Currently, I am an undergraduate who is studying for a bachelor's in the field of ICT at the University of Sri Jayawardenapura.
+                   I am an enthusiastic person who is carving to learn new things and face challenges. Thank you 
+                   <br/>
+                       <p> <a href='thathsaranisiriwardena@gmail.com'>  contact me </a> </p>
+                </p>
+                </div>
+                </div>                
+            </div> 
     </div>
     
   );
